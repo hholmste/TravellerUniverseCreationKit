2f79fe745da5fd202d1ccab25d9d8cf41e046279
Hakon <harnengh@gmail.com>
2014-01-26 20:28:21 +0100

    system selection is starting to work

--- static/js/tuct.js
+++ static/js/tuct.js
@@ -1,5 +1,6 @@
 var Mustache;
 var currentSector;
+var current_system;
 
 var columns = ["01", "02", "03", "04", "05", "06", "07", "08"];
 var rows = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"];
@@ -24,7 +25,10 @@ function initialize_all_the_things() {
 			var system = document.createElement("div");
 			system.classList.add("system");
 			system.innerHTML = system_id;
-			system.id = "system_" + system_id;
+			system.id = system_id;
+			system.onclick = function() {
+				select_system(system_id);
+			};
 			rowElement.appendChild(system);
 		});
 	});
@@ -40,6 +44,13 @@ function createFreshSubsector() {
 
 function readSubSystem() {
 	var subsector = JSON.parse(this.responseText);
+
+	subsector.selectSystem = function(system_id) {
+		return this.systems.filter(function(candidate){
+			return candidate.Coordinate.coordinate == system_id;
+		})[0];
+	};
+
 	setSectorData(subsector);
 	var saveButton = document.getElementById("save_subsector_button");
 	saveButton.classList.remove("hidden");
@@ -52,7 +63,7 @@ function setSectorData(sectorData) {
 
 	sectorData.systems.forEach(function(system){
 		var id = system.Coordinate.coordinate;
-		var systemHolder = document.getElementById("system_" + id);
+		var systemHolder = document.getElementById(id);
 		if (system.System == "Empty") {
 			systemHolder.classList.remove("populated_system");
 			systemHolder.classList.add("empty_system");
@@ -63,11 +74,30 @@ function setSectorData(sectorData) {
 	});
 }
 
+function select_system(system_id) {
+	current_system = currentSector.selectSystem(system_id);
+	var system_html;
+	if (current_system.System == "Empty") {
+		system_html = Mustache.render(empty_system_template, current_system);
+	} else {
+		system_html = Mustache.render(system_data_template, current_system);
+	}
+
+	var system_element = document.getElementById("selected_system");
+	system_element.innerHTML = system_html;
+}
 
 
 var sectorDataTemplate = '<div class="sectorinfo">'
-													+ '<div class="sectorinfoline">Subsector <span class="namespan sectorinfopoint" contentEditable="true">{{Name.name}}</span></div>'
-													+ '<div class="sectorinfoline">Populated worlds: <span class="populatedworldsspan sectorinfopoint">{{PopulatedWorlds}}</span></div>'
-													+ '<div class="sectorinfoline">Average population level <span class="avgpopulation sectorinfopoint">{{AveragePopulation}}</span></div>'
-													+ '<div class="sectorinfoline">Average tech level: <span class="avgtechlevel sectorinfopoint">{{AverageTechLevel}}</span></div>'
+													+ '<div class="infoline">Subsector <span class="namespan infopoint" contentEditable="true">{{Name.name}}</span></div>'
+													+ '<div class="infoline">Populated worlds: <span class="populatedworldsspan infopoint">{{PopulatedWorlds}}</span></div>'
+													+ '<div class="infoline">Average population level <span class="avgpopulation infopoint">{{AveragePopulation}}</span></div>'
+													+ '<div class="infoline">Average tech level: <span class="avgtechlevel infopoint">{{AverageTechLevel}}</span></div>'
 													+ '</div>';
+
+var system_data_template = '<div class="systeminfo">'
+													+ '<div class="infoline">System <span class="namespan infopoint" contentEditable="true">{{System.Name.name}}</span></div>'
+													+ '<div class="infoline">Travelcode <span class="infopoint tc_{{System.TravelCode}}">{{System.TravelCode}}</span></div>'
+													+ '</div>';
+
+var empty_system_template = '<div class="systeminfo"><div class="systeminfoline emptysysteminfo">Nothing at {{Coordinate.coordinate}}</div></div>';
